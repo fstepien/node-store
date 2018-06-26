0b55130bc4f8284d12fa8cfc4080599ecb7d0adb
Filip Stepien <contact@filipstepien.com>
2018-06-26 15:47:34 -0400

    +Schema +Controller w\ POST + upload pug form

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const storeController = require("../controllers/storeController");
+const { catchErrors } = require("../handlers/errorHandlers");
 
 // Do work here
-router.get("/", (req, res) => {
-  res.render("hello", {
-    name: "wes",
-    dog: "snciers"
-  });
-});
-
-router.get("/reverse/:name", (req, res) => {
-  const reverse = [...req.params.name].reverse().join("");
-  res.send(reverse);
-});
+router.get("/", storeController.homePage);
+router.get("/add", storeController.addStore);
+router.post("/add", catchErrors(storeController.createStore));
 
 module.exports = router;
